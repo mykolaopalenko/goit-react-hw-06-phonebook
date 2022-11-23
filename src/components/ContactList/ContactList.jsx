634dcfd08537bf8efbdd92f5bd763ac9c43576ad
Mykola Opalenko <mykola.opalenko@gmail.com>
2022-11-23 12:00:16 +0200

    fix

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { getValueFilter } from '../../redux/Filters/filtersSelectors';
+import { getContacts } from '../../redux/Contacts/contactsSelectors';
+import { useSelector } from 'react-redux';
+import ContactItem from '../ContactItem/ContactItem';
+import PropTypes from 'prop-types';
+import { Item } from './ContactList.styled';
+
+const ContactList = () => {
+  const contacts = useSelector(getContacts);
+  const filterValue = useSelector(getValueFilter);
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filterValue.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <ul>
+      {visibleContacts.map(({ id, name, number }) => (
+        <Item key={id}>
+          <ContactItem id={id} name={name} number={number} />
+        </Item>
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+};
+
+export default ContactList;
